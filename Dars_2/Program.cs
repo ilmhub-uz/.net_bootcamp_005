e3c666e82bd62212f457ad45d5bf470fa47b9e6e
Furqat-DevDo <furqata93@gmail.com>
2023-06-05 21:57:54 +0500

    Dars 2 qo'shildi if else ko'rildi

---
+++ Dars_2/Program.cs
@@ -0,0 +1,48 @@
+﻿//--------------------------if else elseif------------------------------------------------
+
+int score = 85;
+
+if (score >= 90)
+{
+    Console.WriteLine("Excellent!");
+}
+else if (score >= 80)
+{
+    Console.WriteLine("Very good!");
+}
+else if (score >= 70)
+{
+    Console.WriteLine("Good!");
+}
+else if (score >= 60)
+{
+    Console.WriteLine("Pass!");
+}
+else
+{
+    Console.WriteLine("Fail!");
+}
+
+
+if (score >= 90)
+{
+    Console.WriteLine("Excellent!");
+}
+if (score >= 80)
+{
+    Console.WriteLine("Very good!");
+}
+if (score >= 70)
+{
+    Console.WriteLine("Good!");
+}
+if (score >= 60)
+{
+    Console.WriteLine("Pass!");
+}
+else
+{
+    Console.WriteLine("Fail!");
+}
+
+Console.ReadKey();
