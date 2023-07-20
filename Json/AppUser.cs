a1bef28afe172fc08b868d98243b35dfdaff191c
Furqat-DevDo <furqata93@gmail.com>
2023-07-20 21:56:56 +0500

    Jsons

---
+++ Json/AppUser.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Json;
+
+public class AppUser
+{
+    [JsonPropertyName("Name")]
+    public string? FullName { get; set; }
+
+    [JsonPropertyName("Email")]
+    public string? EmailAdrress { get; set; }
+
+    [JsonPropertyName("Phone")]
+    public required string PhoneNumber { get; set; }
+    [JsonPropertyName("Parol")]
+    public required string Password { get; set; }
+
+    public override string ToString()
+        => $" FullName : {FullName}\n Email : {EmailAdrress}" +
+           $"\n Phone : {PhoneNumber}\n PassWord : {Password}";
+
+}
---
+++ Json/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Text.Json;
+using Json;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+var user = new AppUser
+{
+    FullName = "Samandar Xamroqulov",
+    EmailAdrress = null,
+    Password = "1234655",
+    PhoneNumber = "998777878"
+};
+
+
+//var bytes = JsonSerializer.SerializeToUtf8Bytes(user);
+
+//foreach (var by in bytes)
+//{
+//    Console.WriteLine(by);
+//}
+
+//var options = new JsonSerializerOptions
+//{
+//    WriteIndented = true
+//};
+
+//var str = JsonSerializer.Serialize(user, options);
+
+//Console.WriteLine(str);
+
+//var jsonString = @"
+//{
+//    ""Name"": ""Charles"",
+//    ""Phone"": ""+998998773074"",
+//    ""Email"": ""MyEmail"",
+//    ""Parol"" : ""1234""
+//}";
+
+//// ========================================= Deserialize ====================================//
+//var copyObject = JsonSerializer.Deserialize<AppUser>(jsonString) 
+//                 ?? throw new ArgumentNullException();
+//Console.WriteLine(copyObject.ToString());
+
+
+// =============== Using NewtonSoft Json ====================================================//
+
+
+//var jsonObject = JsonConvert.SerializeObject(user,Formatting.Indented)
+//    ?? throw new ArgumentNullException();
+
+//Console.WriteLine(jsonObject);
+
+//var  person = JsonConvert.DeserializeObject<AppUser>(jsonObject) ??
+//              throw new ArgumentNullException();
+
+//Console.WriteLine(person);
+
+// ========================= == = = = Parsing to Json Object ====================== ===//
+
+var jsonString = @"
+{
+    ""Name"": ""Charles"",
+    ""Phone"": ""+998998773074"",
+    ""Email"": ""MyEmail"",
+    ""Parol"" : ""1234""
+}";
+
+JObject jObject = JObject.Parse(jsonString);
+
+string? name = (string?)jObject["Name"];
+string? phone = (string?)jObject["Phone"];
+string? email = (string?)jObject["Email"];
+string? parol = (string?)jObject["Phone"];
+
+
+Console.WriteLine($"Name: {name}");
+Console.WriteLine($"Phone: {phone}");
+Console.WriteLine($"Email: {email}");
+Console.WriteLine($"Parol: {parol}");
